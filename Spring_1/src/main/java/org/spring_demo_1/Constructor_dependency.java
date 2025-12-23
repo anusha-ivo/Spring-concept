0b95e6291fa5a14e611d6a34619c02b13cdac383
anusha-ivo <anusha525@gmail.com>
2025-12-23 20:06:32 +0530

    spring

---
+++ Spring_1/src/main/java/org/spring_demo_1/Constructor_dependency.java
@@ -0,0 +1,4 @@
+package org.spring_demo_1;
+
+public class Constructor_dependency {
+}
---
+++ Spring_1/src/main/java/org/spring_demo_1/SpringCreation_1.java
@@ -0,0 +1,11 @@
+package Spring_demo_1;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class SpringCreation_1 {
+    public static void main(String[] args) {
+            SpringApplication.run(SpringCreation_1.class, args);
+        }
+    }
+
+
