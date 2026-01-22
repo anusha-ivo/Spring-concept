3393c82efb6190db98f535ba1c1c707b786ef5b4
anusha-ivo <anusha525@gmail.com>
2026-01-22 23:53:28 +0530

    transaction

---
+++ spring_jdbc_3/src/test/java/com/example/spring_jdbc_3/SpringJdbc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_jdbc_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
